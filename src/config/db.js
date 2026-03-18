97933f3c44f4f4b753565ebf26aa1bc807ce11c5
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-18 20:41:40 -0300

    feat: add MongoDB connection utility using Mongoose

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const mongoURI = process.env.MONGO_URI || 'mongodb://mongo:27017/moviecatalog';
+
+        const conn = await mongoose.connect(mongoURI);
+
+        console.log(`MongoDB Conectado: ${conn.connect.host}`)
+    } catch (error) {
+        console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
