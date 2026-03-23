07c0a5998efb6fef26b002aaf2bf5daf8c9d4b77
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-23 18:40:38 -0300

    feat: add movie route for top-rated

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import tmdb from '../services/tmdb.service.js';
 import { getMovieDetails } from '../controllers/movie.controller.js';
 
@@ -9,8 +9,18 @@ router.get('/popular', async (req, res) => {
         const response = await tmdb.get('/movie/popular');
         res.json(response.data);
     } catch (error) {
-        console.error("Erro TMDB:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: 'Erro ao buscar filmes do TMDB' });
+        console.error("Erro TMDB Popular: ", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: 'Erro ao buscar filmes populares do TMDB' });
+    }
+});
+
+router.get('/top_rated', async (req, res) => {
+    try {
+        const response = await tmdb.get('/movie/top_rated');
+        res.json(response.data);
+    } catch (error) {
+        console.error("Erro TMDB Top Rated: ", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: 'Erro ao buscar filmes melhores avaliados do TMDB'});
     }
 });
 
