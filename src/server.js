cc56329b58b74b613f4281c7f2941c459b27ab6a
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-31 22:08:15 -0300

    chore: enable CORS for GitHub Pages frontend

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://pedroavilar.github.io'],
     credentials: true
 }));
 
