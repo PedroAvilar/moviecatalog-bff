fbfa491caecab6b22f0dadc2742b77bff5e0c3b4
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-02 16:54:29 -0300

    refactor: remove business rules and data validations from the movie controller

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,57 +1,8 @@
-import tmdb from '../services/tmdb.service.js';
-import Review from '../models/review.model.js';
+import { getMovieDetailsService } from '../services/movie.service.js';
 import asyncHandler from '../utils/asyncHandler.js';
-import AppError from '../utils/AppError.js';
 
 export const getMovieDetails = asyncHandler(async (req, res) => {
-    const { id } = req.params;
+    const response = await getMovieDetailsService(req.params.id);
 
-    if (!id) {
-        throw new AppError('O ID do filme é obrigatório', 400);
-    }
-
-    let detailsRes, creditsRes, reviews;
-
-    try {
-        [detailsRes, creditsRes, reviews] = await Promise.all([
-            tmdb.get(`/movie/${id}`),
-            tmdb.get(`/movie/${id}/credits`),
-            Review.find({ movieId: id }).populate('userId', 'name')
-        ]);
-    } catch (error) {
-        throw new AppError('Filme não encontrado', 404);
-    }
-
-    const movie = detailsRes.data;
-
-    const directors = creditsRes.data.crew
-        .filter(member => member.job === 'Director')
-        .map(d => d.name);
-
-    const cast = creditsRes.data.cast
-        .slice(0, 15)
-        .map(actor => ({
-            id: actor.id,
-            name: actor.name,
-            character: actor.character,
-            profile_path: actor.profile_path || ""
-    }));
-
-    const movieData = {
-        id: movie.id,
-        title: movie.title,
-        overview: movie.overview || "",
-        poster_path: movie.poster_path || "",
-        backdrop_path: movie.backdrop_path || "",
-        release_date: movie.release_date || "",
-        vote_average: movie.vote_average || 0,
-        genres: movie.genres || [],
-        runtime: movie.runtime || 0,
-        directors: directors || [],
-        cast: cast || [],
-        production_companies: movie.production_companies?.map(c => c.name) || [],
-        reviews: reviews || []
-    };
-
-    res.json(movieData);
+    res.json(response);
 });
