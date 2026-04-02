e65e032814c237ff6dcc38fee1bd4236cad2ec3b
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-02 15:41:55 -0300

    feat: implement a favorite service for business rules and formatted data

---
+++ src/services/favorite.service.js
@@ -0,0 +1,48 @@
+import Favorite from '../models/favorite.model.js';
+import AppError from '../utils/AppError.js';
+
+export const toggleFavoriteService = async (userId, data) => {
+    const { movieId, title, poster_path, vote_average } = data;
+
+    if (!movieId) {
+        throw new AppError('O ID do filme é obrigatório', 400);
+    }
+
+    const existing = await Favorite.findOne({ userId, movieId });
+
+    if (existing) {
+        await Favorite.findByIdAndDelete(existing._id);
+
+        return { 
+            message: "Removido dos favoritos", 
+            isFavorite: false 
+        };
+    }
+
+    await Favorite.create({
+        userId,
+        movieId,
+        title,
+        poster_path,
+        vote_average
+    });
+
+    return {
+        message: 'Adicionado aos favoritos',
+        isFavorite: true
+    }
+};
+
+export const getFavoritesService = async (userId) => {
+    const favorites = await Favorite
+        .find({ userId })
+        .sort({ createdAt: -1 });
+    
+    return favorites.map(fav => ({
+        id: fav.movieId,
+        title: fav.title,
+        poster_path: fav.poster_path,
+        vote_average: fav.vote_average,
+        createdAt: fav.createdAt
+    }));
+};
