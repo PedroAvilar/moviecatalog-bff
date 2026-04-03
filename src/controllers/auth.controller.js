ff08edfb88cd8321f499237153272b0935d1e198
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-02 21:06:57 -0300

    refactor: separating getMe from route for controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,6 +42,10 @@ export const login = asyncHandler(async (req, res) => {
     });
 });
 
+export const getMe = (req, res) => {
+    res.json(req.user);
+}
+
 export const updateProfile = asyncHandler(async (req, res) => {
     const user = await updateUserProfile(req.user.id, req.body);
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteAccount, login, logout, register, updatePassword, updateProfile } from '../controllers/auth.controller.js';
+import { deleteAccount, getMe, login, logout, register, updatePassword, updateProfile } from '../controllers/auth.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -7,9 +7,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get('/me', protect, (req, res) => {
-    res.json(req.user);
-});
+router.get('/me', protect, getMe);
 router.put('/update-profile', protect, updateProfile);
 router.put('/update-password', protect, updatePassword);
 router.delete('/delete-account', protect, deleteAccount);
