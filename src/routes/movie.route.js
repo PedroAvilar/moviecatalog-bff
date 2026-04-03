bb4dc92cb67f2e6287443ef65577c0f24f6f342e
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-02 21:08:43 -0300

    style: remove unnecessary line breaks

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -4,13 +4,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/:id/details', getMovieDetails);
-
 router.get('/popular', getPopularMovies);
-
 router.get('/top_rated', getTopRatedMovies);
-
 router.get('/genres', getGenresMovies);
-
 router.get('/discover', discoverMovies);
 
 export default router;
