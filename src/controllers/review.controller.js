a8f124f2c3d83f414f3ecbdc36c35a64a0920a31
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-15 17:15:36 -0300

    refactor: add sendResponse to review.controller responses

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,26 +1,39 @@
 import { createReviewService, deleteReviewService, getMyReviewsService, updateReviewService } from '../services/review.service.js';
 import asyncHandler from '../utils/asyncHandler.js';
+import { sendSuccess } from '../utils/sendResponse.js';
 
 export const createReview = asyncHandler(async (req, res) => {
     await createReviewService(req.user.id, req.body);
 
-    res.json({ message: 'Avaliação criada com sucesso' });
+    return sendSuccess(
+        res,
+        {},
+        'Avaliação criada com sucesso'
+    );
 });
 
 export const getMyReviews = asyncHandler(async (req, res) => {
     const reviews = await getMyReviewsService(req.user.id);
-    
+
     res.json(reviews);
 });
 
 export const deleteReview = asyncHandler(async (req, res) => {
     await deleteReviewService(req.user.id, req.params.id)
-    
-    res.json({ message: 'Avaliação removida com sucesso' });
+
+    return sendSuccess(
+        res,
+        {},
+        'Avaliação removida com sucesso'
+    );
 });
 
 export const updateReview = asyncHandler(async (req, res) => {
     await updateReviewService(req.user.id, req.params.id, req.body);
 
-    res.json({ message: 'Avaliação alterada com sucesso' });
+    return sendSuccess(
+        res,
+        {},
+        'Avaliação alterada com sucesso'
+    );
 });
