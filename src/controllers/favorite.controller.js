a061491227c4d432e04027b5ec19556f8b663990
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-15 17:14:51 -0300

    refactor: add sendResponse to favorite.controller responses

--- src/controllers/favorite.controller.js
+++ src/controllers/favorite.controller.js
@@ -1,10 +1,16 @@
 import { getFavoritesService, toggleFavoriteService } from '../services/favorite.service.js';
+import { sendSuccess } from '../utils/sendResponse.js';
 import asyncHandler from '../utils/asyncHandler.js';
 
 export const toggleFavorite = asyncHandler(async (req, res) => {
     const result = await toggleFavoriteService(req.user.id, req.body);
-    
-    res.status(result.isFavorite ? 201 : 200).json(result);
+
+    return sendSuccess(
+        res,
+        { isFavorite: result.isFavorite },
+        result.message,
+        result.isFavorite ? 201 : 200
+    );
 });
 
 export const getFavorites = asyncHandler(async (req, res) => {
