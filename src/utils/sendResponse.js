9921354db93098eb402806b57525fa8fded2fd40
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-28 16:43:40 -0300

    fix: prevent overriding message and type in sendResponse

--- src/utils/sendResponse.js
+++ src/utils/sendResponse.js
@@ -1,15 +1,29 @@
 export const sendSuccess = (res, data = {}, message = 'Sucesso', statusCode = 200) => {
+    const safeData = data && typeof data === 'object'
+        ? Object.fromEntries(
+            Object.entries(data).filter(
+                ([key]) => key !== 'message' && key !== 'type'
+            )
+        ) : {};
+
     return res.status(statusCode).json({
         message,
         type: 'success',
-        ...data
+        ...safeData
     });
 };
 
 export const sendError = (res, message = 'Erro interno no servidor', statusCode = 500, extra = {}) => {
+    const safeExtra = extra && typeof extra === 'object'
+        ? Object.fromEntries(
+            Object.entries(extra).filter(
+                ([key]) => key !== 'message' && key !== 'type'
+            )
+        ) : {};
+
     return res.status(statusCode).json({
         message,
         type: 'error',
-        ...(extra || {})
+        ...safeExtra
     });
 };
